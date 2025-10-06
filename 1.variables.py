ef879a73cbcac77450166708ae68870129f1e523
Mohd Rehan <mdrehanchhipa@gmail.com>
2025-10-06 14:30:21 +0530

    learn about datatypes in python

---
+++ 1.variables.py
@@ -0,0 +1,42 @@
+print("Assalamualikum Python")
+
+#This is a single line comment in python
+
+
+"""
+This 
+is the
+Multiline
+Comment in Python
+
+"""
+
+name = "Rehan"
+name = "Omar" #Here the variable 'name' has overridden.
+print(name)
+
+"""
+Naming Conventions:
+1.Always start you variable name with lowercase or with underscore (_).
+2.We cant use reserved keywords as our variabel name.
+
+Reserved Keywords:
+
+class
+def
+true
+false
+break
+continue
+len
+list
+
+"""
+
+#Python is case sensitive :
+
+Product = "Soap"
+product = "Oil"
+
+print(Product)
+print(product)
